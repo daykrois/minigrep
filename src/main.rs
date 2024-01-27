48c2d6bcae90aba739bdf3e48000645bda97b648
daykrois <saiyanqlwy@foxmail.com>
2024-01-28 04:06:57 +0800

    rust练习

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{env, process};
+
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("参数错误:{err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config){
+        println!("错误:{e}");
+        process::exit(1);
+    };
 }
